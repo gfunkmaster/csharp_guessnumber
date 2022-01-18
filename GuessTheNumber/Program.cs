6d56eabd65d6ee4976107e29e6ea42877f942d3b
gfunkmaster <nurkicelmin@hotmail.com>
2022-01-18 15:36:53 +0100

    Small project, guess the right number

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -6,7 +6,26 @@ namespace GuessTheNumber
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Random rand = new Random();
+            int answer = rand.Next(0, 100);
+            int guess = -1;
+
+            while(guess != answer)
+            {
+                Console.WriteLine("Guess the right number?");
+                guess = int.Parse(Console.ReadLine());
+                if(guess > answer)
+                {
+                    Console.WriteLine("Too high");
+                } else if(guess < answer)
+                {
+                    Console.WriteLine("Too low");
+                }
+
+            }
+            Console.WriteLine($"You guess the right answer:  {answer}");
+            
+
+         }
     }
 }
